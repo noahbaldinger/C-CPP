96c4d7d6559101a5a544edc4ba4dc009fb5e7982
Noah Baldinger <baldingernoah@gmail.com>
2023-08-04 15:38:06 +0200

    some cleaning up

--- password.cpp
+++ password.cpp
@@ -1,31 +1,31 @@
-#include <iostream>
-#include <ctime>
 #include <cstdlib>
+#include <ctime>
+#include <iostream>
 #include <string>
 
 std::string generateRandomPassword(int length) {
-    const std::string charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,./?!@#$%^&*()_+-=[]{};:<>";
-    std::string password = "";
-    int charsetSize = static_cast<int>(charset.size());
+  const std::string charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUV"
+                              "WXYZ0123456789,./?!@#$%^&*()_+-=[]{};:<>";
+  std::string password = "";
+  int charsetSize = static_cast<int>(charset.size());
 
-    for (int i = 0; i < length; i++) {
-        int randomIndex = rand() % charsetSize;
-        password += charset[randomIndex];
-    }
+  for (int i = 0; i < length; i++) {
+    int randomIndex = rand() % charsetSize;
+    password += charset[randomIndex];
+  }
 
-    return password;
+  return password;
 }
 
 int main() {
-    // Seed the random number generator with the current time
-    srand(static_cast<unsigned>(time(0)));
+  // Seed the random number generator with the current time
+  srand(static_cast<unsigned>(time(0)));
 
-    // Generate a 20-character long random password
-    int passwordLength = 30;
-    std::string password = generateRandomPassword(passwordLength);
+  // Generate a 20-character long random password
+  int passwordLength = 30;
+  std::string password = generateRandomPassword(passwordLength);
 
-    std::cout << "Generated Password: " << password << std::endl;
+  std::cout << "Generated Password: " << password << std::endl;
 
-    return 0;
+  return 0;
 }
-
